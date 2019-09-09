2710d4bcf871d20e100c77e38a89570db535865b
henrisja <henrisja@umich.edu>
2019-09-09 13:46:21 -0400

    Added classwork file

---
+++ classwork.py
@@ -0,0 +1,11 @@
+# matt, kelsy, adley, josh, jake
+
+names = ['matt', 'kelsy', 'adley', 'josh', 'jake']
+names.sort()
+first_name = names[0]
+
+# [:-1] gives everything up to the last character of the string
+first_name = first_name[:-1]
+print(first_name)
+
+# just loop through and get the longest here
